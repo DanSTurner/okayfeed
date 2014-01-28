1c207a75c876519aad41f783fb23f554d3389647
Dan S Turner <turner@nowwithmoredan.com>
2014-01-28 10:20:42 -0800

    New rails app initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Okayfeed::Application.config.secret_key_base = 'b44c64a9cbd7b1d35d65c30a00ec24f8fc90795135fd6ff81d46237dedbca76c28894aa7994821ccaee99b092a5228e3dd6b436bc7d908f95590c275aca11ddc'
