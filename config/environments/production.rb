73d95dd84a465b4cae3e6d6a573f3323713efcb2
Dan S Turner <turner@nowwithmoredan.com>
2014-03-13 13:53:13 -0700

    sendgrid config complete

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,7 +78,7 @@ Okayfeed::Application.configure do
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
 
-  config.action_mailer.default_url_options = { :host => 'okayfeed-staging.heroku.com'}
+  config.action_mailer.default_url_options = { :host => ENV["MAILER_DEFAULT_URL_HOST"]}
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
     :port           => '587',
